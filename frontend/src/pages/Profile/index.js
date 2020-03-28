061e5d9888232514feab27e3ed13c8336c0aa52e
Israel <israelwebti@gmail.com>
2020-03-28 15:49:50 -0300

    Criando Rota de Login

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -4,11 +4,12 @@ import { FiPower, FiTrash2 } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 import './style.css';
 export default function Profile() {
+  const ongName = localStorage.getItem('ongName');
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Be Hero" />
-        <span>Bem vinda, APAD</span>
+        <span>Bem vinda, {ongName}</span>
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
         <button type="button">
           <FiPower size={18} color="#E02041" />
