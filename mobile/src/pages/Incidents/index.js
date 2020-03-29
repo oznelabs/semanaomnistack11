6ceaa63b9a95c91bf108878065856dcbfa66c9e9
Israel <israelwebti@gmail.com>
2020-03-29 20:39:47 -0300

    Improving FlatList in APP Incidents and connecting api service

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native'
 import {
@@ -11,13 +11,22 @@ import {
 } from 'react-native';
 import logoImg from '../../assets/logo.png';
 
+import api from '../../services/api';
 import style from './style';
 
 export default function Incidents() {
+  const [incidents, setInsidents] = useState([]);
   const navigation = useNavigation();
   function navigateToDetail() {
     navigation.navigate('Detail');
   }
+  async function loadIncidents() {
+    const res = await api.get('/incidents');
+    setInsidents(res.data);
+  }
+  useEffect(() => {
+    loadIncidents();
+  }, []);
   return (
     <View style={style.container}>
       <View style={style.header}>
@@ -30,32 +39,29 @@ export default function Incidents() {
       <Text style={style.description}>
         Escolha um dos casos abaixo e salve o dia!
       </Text>
-
-      <SafeAreaView>
-        <FlatList
-          data={[1, 2, 3, 4, 5]}
-          style={style.incidentList}
-          showsVerticalScrollIndicator={false}
-          renderItem={({ item }) => (
-            <View key={item.valueOf()} style={style.incident}>
-              <Text style={style.incidentProperty}>ONGs: </Text>
-              <Text style={style.incidentValue}>VivaBarra</Text>
-              <Text style={style.incidentProperty}>CASO: </Text>
-              <Text style={style.incidentValue}>Comprar livros</Text>
-              <Text style={style.incidentProperty}>VALOR: </Text>
-              <Text style={style.incidentValue}>R$ 120,00</Text>
-              <TouchableOpacity
-                style={style.detailsButton}
-                onPress={navigateToDetail}
-              >
-                <Text style={style.detailsButtonText}>Ver mais detalhes</Text>
-                <Feather name="arrow-right" size={16} color="#e02041" />
-              </TouchableOpacity>
-            </View>
-          )}
-          keyExtractor={item => item.id}
-        />
-      </SafeAreaView>
+      <FlatList
+        data={incidents}
+        style={style.incidentList}
+        keyExtractor={incident => String(incident.id)}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item: incident }) => (
+          <View key={incident.id} style={style.incident}>
+            <Text style={style.incidentProperty}>ONGs: </Text>
+            <Text style={style.incidentValue}>{incident.name}</Text>
+            <Text style={style.incidentProperty}>CASO: </Text>
+            <Text style={style.incidentValue}>{incident.title}</Text>
+            <Text style={style.incidentProperty}>VALOR: </Text>
+            <Text style={style.incidentValue}>{incident.value}</Text>
+            <TouchableOpacity
+              style={style.detailsButton}
+              onPress={navigateToDetail}
+            >
+              <Text style={style.detailsButtonText}>Ver mais detalhes</Text>
+              <Feather name="arrow-right" size={16} color="#e02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
 
     </View>
   )
